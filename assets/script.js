db17e2dd895b22dc68ed32c5613d3fd254a1e34d
Aman kumar keshri <72082692+aman5062@users.noreply.github.com>
2025-04-15 10:27:38 +0530

    first commit

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+  var adminUsername = "admin";
+  var adminPassword = "admin123";
+  var loginContainer = document.getElementById("loginContainer");
+  var dashboard = document.getElementById("dashboard");
+  var recordsTable = document.getElementById("recordsTable").querySelector("tbody");
+  var sideMenu = document.getElementById("sideMenu");
+    var nav_bar = document.getElementById("nav_bar")
+
+    function login() {
+        const user = document.getElementById("username").value;
+        const pass = document.getElementById("password").value;
+      
+        if (user === adminUsername && pass === adminPassword) {
+          loginContainer.classList.add("hidden");
+          dashboard.classList.remove("hidden");
+          document.getElementById("navbarContainer").classList.remove("hidden");
+        } else {
+          alert("Invalid credentials!");
+        }
+      }
+      
+
+  function logout() {
+  dashboard.classList.add("hidden");
+  loginContainer.classList.remove("hidden");
+  sideMenu.classList.remove("show");
+  document.getElementById("navbarContainer").classList.add("hidden");
+}
+
+
+  function markAttendance() {
+    var name = document.getElementById("studentName").value.trim();
+    var status = document.getElementById("status").value;
+    var date = new Date().toLocaleDateString();
+
+    if (name === "") {
+      alert("Please enter student name.");
+      return;
+    }
+
+    var row = recordsTable.insertRow();
+    row.insertCell(0).innerText = name;
+    row.insertCell(1).innerText = status;
+    row.insertCell(2).innerText = date;
+
+    document.getElementById("studentName").value = "";
+  }
+
+  function toggleMenu() {
+    sideMenu.classList.toggle("show");
+  }
+
+  function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    toggleMenu();
+  }
